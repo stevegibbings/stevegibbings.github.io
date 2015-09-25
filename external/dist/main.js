e5a86c06ed778f6416afdd9e94b0bcc5d508190c
steve gibbings <me@stevegibbings.co.uk>
2015-09-25 14:19:49 +0100

    corrected paths

--- external/dist/main.js
+++ external/dist/main.js
@@ -2,7 +2,7 @@
  * Created by steve.gibbings on 25/09/2015.
  */
 require.config({
-    baseUrl: "/external/dist",
+    baseUrl: "dist",
     paths: {
           "lazysizes": "https://cdn.rawgit.com/aFarkas/lazysizes/gh-pages/lazysizes.min",
           "picturefill": "https://cdnjs.cloudflare.com/ajax/libs/picturefill/2.3.1/picturefill"
